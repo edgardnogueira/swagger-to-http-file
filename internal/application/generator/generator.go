f3dff364010c66824f54869e07bf95e500d3b1b9
Edgard Nogueira <edgard.nogueira@gmail.com>
2025-04-16 21:26:04 +0100

    feat(issue-2): update http generator interface

--- internal/application/generator/generator.go
+++ internal/application/generator/generator.go
@@ -6,7 +6,15 @@ import (
 
 // HttpGenerator defines the interface for generating HTTP files from Swagger documents
 type HttpGenerator interface {
-	Generate(doc *models.SwaggerDoc) (map[string]*models.HttpFile, error)
-}
+	// Generate creates HTTP files from a Swagger document
+	Generate(doc *models.SwaggerDoc, baseURL string) (map[string]*models.HttpFile, error)
 
-// Basic implementation will be added during issue implementation
+	// GenerateRequest creates a single HTTP request from an operation
+	GenerateRequest(op models.OperationInfo, baseURL string) models.HttpRequest
+
+	// FormatPath formats path parameters for use in a request URL
+	FormatPath(path string, params []models.Parameter) string
+
+	// ExtractGlobalVars extracts global variables that could be used across requests
+	ExtractGlobalVars(doc *models.SwaggerDoc) map[string]string
+}
